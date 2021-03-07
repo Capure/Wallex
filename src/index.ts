4d9636ce42f8feb1b42a1c8bb42fed98dfb1bf38
capure <stolarek83@gmail.com>
2021-03-07 10:33:02 +0100

    :sparkles: Added custom wallpaper support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {app, Menu, Tray, BrowserWindow, screen, MenuItemConstructorOptions} from 'electron';
+import {app, Menu, Tray, BrowserWindow, screen, MenuItemConstructorOptions, dialog} from 'electron';
 import electronWallpaper from '../electronWallpaper';
 import path from 'path';
-
+import fs from 'fs';
 
 app.allowRendererProcessReuse = true;
 
@@ -10,7 +10,30 @@ let tray: Tray | null;
 let currentScreen: Electron.Display | null;
 let currentScreenIdx: number | null;
 
+interface Wallpaper {
+  path: string,
+  name: string
+}
+
 let screens: Electron.Display[] | null;
+const pathToData = app.getPath('userData');
+const pathsToWallpapers: Wallpaper[] = [];
+
+const loadWallpapers = () => {
+  try {
+    const dirRaw = fs.readdirSync(path.join(pathToData, 'wallpapers'), { withFileTypes: true });
+    dirRaw.forEach((dir: fs.Dirent) => {
+      if (!dir.isDirectory()) {
+        return;
+      }
+      pathsToWallpapers.push({path: path.join(pathToData, 'wallpapers', dir.name, 'index.html'), name: dir.name});
+    });
+  } catch {
+    fs.mkdirSync(path.join(pathToData, 'wallpapers'));
+  }
+}
+
+loadWallpapers(); // Initial load
 
 const getOffsetX = () => {
   if (!currentScreen || currentScreenIdx === null || !screens) {
@@ -31,7 +54,7 @@ const destroyWallpaper = (idx: number) => {
   }
 }
 
-const createWallpaperWindow = function() {
+const createWallpaperWindow = function(pathToWallpaper: string) {
   if (!currentScreen || currentScreenIdx === null || !screens) {
     throw Error("Current screen is unset!");
   }
@@ -48,19 +71,19 @@ const createWallpaperWindow = function() {
     // }, // 3 fucking lines cuz electron is "SECURE" now
     // The lines above allow node modules in wallpapers
   });
-  wallpaperWindows[currentScreenIdx].loadFile('../public/dashboard.html');
+  wallpaperWindows[currentScreenIdx].loadFile(pathToWallpaper);
   electronWallpaper.attachWindow(wallpaperWindows[currentScreenIdx], getOffsetX(),
     currentScreen.bounds.y, currentScreen.bounds.width, currentScreen.bounds.height);
 };
 
-const setCurrentScreen = (idx: number) => {
+const setCurrentScreen = (idx: number, pathToWallpaper: string) => {
   if (!screens) {
     throw Error("Screens hasn't been initialized!");
   }
   currentScreen = screens[idx];
   currentScreenIdx = idx;
   destroyWallpaper(idx);
-  createWallpaperWindow();
+  createWallpaperWindow(pathToWallpaper);
 }
 
 const createTray = () => {
@@ -69,19 +92,16 @@ const createTray = () => {
   }
   tray = new Tray(path.join(__dirname, '../public/logo.png'));
   const ctxMenu = Menu.buildFromTemplate([
-    // { label: 'display', type: 'submenu', submenu: [
-    //   ...(screens.map((_, idx): MenuItemConstructorOptions => ({
-    //     label: idx.toString(),
-    //     type: 'normal',
-    //     click: () => setCurrentScreen(idx)
-    //   })))
-    // ] }, // This creates a menu entry for every monitor available in the system. TODO: allow multiply monitors
     ...(screens.map((_, idx): MenuItemConstructorOptions => {
       return { label: `Screen ${idx+1}`, type: 'submenu', submenu: [
         { label: 'Disable', type: 'normal', click: () => destroyWallpaper(idx) },
-        { label: 'Default', type: 'normal', click: () => setCurrentScreen(idx) }
+        ...(pathsToWallpapers.map((item): MenuItemConstructorOptions => ({ label: item.name, type: 'normal', click: () => setCurrentScreen(idx, item.path) })))
       ] }
-    })),
+    })), // Allows the user to pick a wallpaper for each screen
+    { label: 'wallpapers', type: 'normal', click: () => {
+      dialog.showOpenDialog({ title: 'Add your wallpaper', defaultPath: path.join(pathToData, 'wallpapers'),
+      buttonLabel: '-->' }).then(() => { pathsToWallpapers.length = 0; loadWallpapers(); tray?.destroy(); createTray() }); // Should refresh the wallpapers
+    } },
     { label: 'quit', type: 'normal', click: () => { 
       wallpaperWindows.map((_, idx) => destroyWallpaper(idx));
       app.quit();
