1a8e973a82f4d5bfa4f7e95283ad212e36331478
Kacper Stolarek <stolarek83@gmail.com>
2024-08-15 18:40:05 +0200

    Refactored tray to class and fixed opening the wallpaper folder

--- src/createTray.ts
+++ src/createTray.ts
@@ -1,4 +1,6 @@
 import {
+  App,
+  shell,
   Display,
   Menu,
   MenuItemConstructorOptions,
@@ -7,6 +9,7 @@ import {
 } from "electron";
 import path from "path";
 import { getAllDisplays } from "./screenUtils";
+import { WallpaperManager } from "./wallpaperManager";
 
 const ICON_PATH = path.join(__dirname, "../public/trayTemplate.png");
 
@@ -18,33 +21,66 @@ export interface WallexTrayHandlers {
   onQuit: () => void;
 }
 
-const buildDisplayMenuTemplate: (
-  display: Display
-) => MenuItemConstructorOptions | Electron.MenuItem = (display: Display) => {
-  return { label: display.label, type: "submenu", submenu: [] };
-};
 
-const createTemplate = (handlers: WallexTrayHandlers) => {
-  let template: (MenuItemConstructorOptions | Electron.MenuItem)[] = [];
-  template = [...getAllDisplays().map(buildDisplayMenuTemplate)];
-  template.push({ type: "separator" });
-  template.push({
-    label: "Open the wallpapers folder",
-    click: () => handlers.onOpenWallpaperFolder(),
-  });
-  template.push({
-    label: "Refresh wallpapers",
-    click: () => handlers.onRefresh(),
-  });
-  template.push({ type: "separator" });
-  template.push({ label: "Quit", click: () => handlers.onQuit() });
-  return template;
-};
+export class WallexTray implements WallexTrayHandlers {
+  private app: App;
+  private wallpaperManager: WallpaperManager;
+  private tray: Tray;
+  constructor(app: App) {
+    this.app = app;
+    this.wallpaperManager = new WallpaperManager(this.app.getPath("userData"));
+    this.tray = this.buildTray();
+  }
 
-export const createTray = (handlers: WallexTrayHandlers) => {
-  const tray = new Tray(icon);
-  const contextMenu = Menu.buildFromTemplate(createTemplate(handlers));
-  tray.setToolTip("Wallex");
-  tray.setContextMenu(contextMenu);
-  return tray;
-};
+  private buildTray() {
+    const tray = new Tray(icon);
+    const contextMenu = Menu.buildFromTemplate(this.createTemplate(this));
+    tray.setToolTip("Wallex");
+    tray.setContextMenu(contextMenu);
+    return tray;
+  }
+
+  // template
+
+  private buildDisplayMenuTemplate(
+    display: Display
+  ): MenuItemConstructorOptions | Electron.MenuItem {
+    const submenu = this.wallpaperManager.getWallpapers().map(wallpaper => ({ label: wallpaper.name, click: () => { } }) as MenuItemConstructorOptions);
+    return { label: display.label, type: "submenu", submenu };
+  };
+
+  private createTemplate(handlers: WallexTrayHandlers) {
+    let template: (MenuItemConstructorOptions | Electron.MenuItem)[] = [];
+
+    const wallexTrayInstance = this;
+    template = [...getAllDisplays().map(this.buildDisplayMenuTemplate.bind(wallexTrayInstance))];
+
+    template.push({ type: "separator" });
+    template.push({
+      label: "Open the wallpapers folder",
+      click: () => handlers.onOpenWallpaperFolder(),
+    });
+    template.push({
+      label: "Refresh wallpapers",
+      click: () => handlers.onRefresh(),
+    });
+    template.push({ type: "separator" });
+    template.push({ label: "Quit", click: () => handlers.onQuit() });
+    return template;
+  };
+
+  // handlers
+
+  public async onOpenWallpaperFolder() {
+    shell.openPath(this.wallpaperManager.getWallpapersFolder());
+  };
+  public onRefresh() {
+    this.wallpaperManager.reload();
+    this.tray.destroy();
+    this.tray = this.buildTray();
+    console.log(this.wallpaperManager.getWallpapers());
+  };
+  public onQuit() {
+    this.app.quit();
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,13 @@
 import { type Tray, app, dialog } from "electron";
-import { createTray } from "./createTray";
+import { WallexTray } from "./createTray";
 import { WallpaperManager } from "./wallpaperManager";
 
 const isOnMac = process.platform === "darwin";
-
-const refreshWallpapers = (tray: Tray, wallpaperManager: WallpaperManager) => {
-  wallpaperManager.reload();
-  tray.destroy();
-  buildTray(wallpaperManager);
-};
-
-const openWallpaperFolder = async (
-  tray: Tray,
-  wallpaperManager: WallpaperManager
-) => {
-  await dialog.showOpenDialog({
-    title: "Add your wallpaper",
-    defaultPath: wallpaperManager.getWallpapersFolder(),
-    properties: ["createDirectory"],
-  });
-  //   refreshWallpapers(tray, wallpaperManager);
-};
-
-const buildTray = (wallpaperManager: WallpaperManager) => {
-  const tray = createTray({
-    onQuit: () => app.quit(),
-    onRefresh: () => refreshWallpapers(tray, wallpaperManager),
-    onOpenWallpaperFolder: () => openWallpaperFolder(tray, wallpaperManager),
-  });
-};
 
 app.whenReady().then(() => {
   if (isOnMac) app.dock.hide();
 
   const wallpaperManager = new WallpaperManager(app.getPath("userData"));
-  console.log(wallpaperManager.getWallpapers());
 
-  buildTray(wallpaperManager);
+  const tray = new WallexTray(app);
 });
